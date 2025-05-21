35fd754f5e0abace597bb002c1b6542879e97f46
Seydou CISSE <cisse.seydou@icloud.com>
2025-05-21 18:52:46 +0100

    improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 
+import sitemap from '@astrojs/sitemap';
+
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://seydoucisse.github.io',
+  integrations: [sitemap()],
 });
